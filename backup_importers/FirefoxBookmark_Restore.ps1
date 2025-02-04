abc7d8e3d3f822704b567b278a39f864d55059d5
Corey <52892773+z3eeK@users.noreply.github.com>
2025-02-04 02:49:38 -0500

    Update FirefoxBookmark_Restore.ps1

--- backup_importers/FirefoxBookmark_Restore.ps1
+++ backup_importers/FirefoxBookmark_Restore.ps1
@@ -1,4 +1,8 @@
-mkdir "C:\Backup_Firefox"
+@echo off
+setlocal
+set BACKUP_DIR=C:\Backup_Firefox
+mkdir "%BACKUP_DIR%"
 $MozillaPlaces = (gci "$env:userprofile\appdata\Roaming\Mozilla\Firefox\Profiles" -force -recurse -ErrorAction SilentlyContinue | ?{$_.Name -eq 'places.sqlite'}).DirectoryName
-Robocopy "C:\Backup_Firefox" "$MozillaPlaces" 'places.sqlite' /s /e /W:1 /R:1 | Out-Null
-pause
+Robocopy "%BACKUP_DIR%" "$MozillaPlaces" 'places.sqlite' /s /e /W:1 /R:1 | Out-Null
+endlocal
+timeout 3
