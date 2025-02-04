7b11c5f23f3cb501936ac9bb3fe94dd272464b33
Corey <52892773+z3eeK@users.noreply.github.com>
2025-02-04 02:48:37 -0500

    Update TaskScheduler_Restore.bat

--- backup_importers/TaskScheduler_Restore.bat
+++ backup_importers/TaskScheduler_Restore.bat
@@ -6,12 +6,13 @@ echo %BACKUP_DIR% does not exist.
 pause
 goto END
 )
-xcopy /E /I /Y "%BACKUP_DIR%" "C:\Windows\System32\Tasks" || goto :error
+xcopy /E /I "%BACKUP_DIR%" "C:\Windows\System32\Tasks" || goto :error
 echo Operation Complete
 goto :END
 :error
 echo Failed with error #%errorlevel%.
 echo Are you sure you ran administrator?
+pause
 :END
 endlocal
-pause
+timeout 3
