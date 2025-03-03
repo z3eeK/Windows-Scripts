a9eb6d976e6ff2ab6e57f477f27c6f27fdb5d782
Corey <52892773+z3eeK@users.noreply.github.com>
2025-03-03 06:06:38 -0500

    Add files via upload

---
+++ optimizers/Full_Integrity_Check.bat
@@ -0,0 +1,31 @@
+sfc /scannow
+Dism /Online /Cleanup-Image /ScanHealth
+Dism /Online /Cleanup-Image /CheckHealth
+DISM /Online /Cleanup-Image /RestoreHealth
+chkdsk /r A:
+chkdsk /r B:
+chkdsk /r D:
+chkdsk /r E:
+chkdsk /r F:
+chkdsk /r G:
+chkdsk /r H:
+chkdsk /r I:
+chkdsk /r J:
+chkdsk /r K:
+chkdsk /r L:
+chkdsk /r M:
+chkdsk /r N:
+chkdsk /r O:
+chkdsk /r P:
+chkdsk /r Q:
+chkdsk /r R:
+chkdsk /r S:
+chkdsk /r T:
+chkdsk /r U:
+chkdsk /r V:
+chkdsk /r W:
+chkdsk /r X:
+chkdsk /r Y:
+chkdsk /r Z:
+C:\Windows\System32\MdSched.exe
+chkdsk /r C:
